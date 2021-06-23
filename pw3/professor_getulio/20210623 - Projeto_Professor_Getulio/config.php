35291ae6013ff08e8a649c391f285541cc8c719d
Jeferson Ferreira <jefender@gmail.com>
2021-06-23 12:48:03 -0300

    Novo Projeto PHP CRUD

---
+++ pw3/professor_getulio/20210623 - Projeto_Professor_Getulio/config.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+//Configuração BD
+$host = "localhost";
+$user = "root";
+$pass = "usbw";
+$d_b = "u671492475_teste";
+
+//////////////////////////////////////
+//Não altere a partir daqui!
+/////////////////////////////////////
+//Estilo orientado a objetos
+$con = new mysqli($host, $user, $pass, $d_b);
+
+$res = mysqli_query($con,"SELECT * FROM fornecedores");
+$pag = mysqli_query($con,"SELECT * FROM pagamentos");
+
+?>
